b007bd6f3726de11397add894a412d84ecc8b74a
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-02-01 21:08:59 +1100

    new design

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,59 @@ console.log('Client side javascript file is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#p2')
-const messageTwo = document.querySelector('#p3')
+search.addEventListener('keypress',setQuery);
+const messageOne = document.querySelector('.city')
+const summary = document.querySelector('.weather')
+const temperature = document.querySelector('.temp')
+const highlow = document.querySelector('.hi-low')
+const date = document.querySelector('.date')
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];  
+function dateFormat1(d) {
+    var t = new Date(d);
+    return n + " " + t.getDate() + ' ' + monthNames[t.getMonth()]  + " " + t.getFullYear();
+  }
+// console.log(dateFormat1(new Date()))
+
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1; //months from 1-12
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
 
 
 
-weatherForm.addEventListener('submit',  (e) => {
-    e.preventDefault()
+newdate = year + "/" + month + "/" + day;
+
+	function setQuery(evt){
+	if(evt.keyCode==13){
     const location = search.value
-
-    messageOne.textContent = 'Loading......'
-    messageTwo.textContent = ''
-
     fetch('/weather?address=' + location).then ((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
         } else {
             messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
+            date.textContent = dateFormat1(new Date())
+            summary.textContent = data.summary
+            temperature.textContent = data.temp + "°c";
+            highlow.textContent = data.low  + "°c / " + data.high + "°c";
             } 
         })
     })
-})  
+	}
+}
+ 
